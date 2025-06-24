79af4eeeb563c0600f75c7dd19f7d8ff4aa0d402
Jabame731 <justinetaglucop@gmail.com>
2025-06-24 10:42:20 +0800

    adding in initial files

---
+++ src/data/dto/user/Register.dto.ts
@@ -0,0 +1,11 @@
+export class RegisterUserDTO {
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public phoneNumber: string,
+    public address: string,
+    public password: string,
+    public userPicture: string
+  ) {}
+}
